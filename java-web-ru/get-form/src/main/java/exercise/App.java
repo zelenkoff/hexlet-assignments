e714672ef9453fa9a5898c0a89cf94c065be33a7
@hexlet/cli <support@hexlet.io>
2023-12-05 10:28:00 +0100

    submit java-web-ru/get-form

---
+++ java-web-ru/get-form/src/main/java/exercise/App.java
@@ -0,0 +1,59 @@
+package exercise;
+
+import io.javalin.Javalin;
+import java.util.List;
+import exercise.model.User;
+import exercise.dto.users.UsersPage;
+import java.util.Collections;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.apache.commons.lang3.StringUtils;
+
+public final class App {
+
+    // Каждый пользователь представлен объектом класса User
+    private static final List<User> USERS = Data.getUsers();
+
+    public static Javalin getApp() {
+
+        var app = Javalin.create(config -> {
+            config.plugins.enableDevLogging();
+        });
+
+        // BEGIN
+
+        app.get("/users", ctx -> {
+            var term = ctx.queryParam("term");
+            List<User> users;
+
+            if (term != null) {
+                users = USERS.stream()
+                        .filter(
+                                u -> u.getFirstName()
+                                        .toLowerCase()
+                                        .startsWith(term.toLowerCase())
+                        ).collect(Collectors.toList());
+            } else {
+                users = USERS;
+            }
+
+            var page = new UsersPage(users, users.get(0).getFirstName() + ' ' + users.get(0).getLastName());
+
+            ctx.render("users/index.jte", Collections.singletonMap("page", page));
+        });
+        
+        // END
+
+        app.get("/", ctx -> {
+            ctx.render("index.jte");
+        });
+
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(7070);
+    }
+}
